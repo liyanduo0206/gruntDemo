d3dc775a7cb5147fee25cfc9deae33f984ae79ac
xlite <liyanduo0206@163.com>
2016-05-19 16:01:05 +0800

    Revert "Revert "csss""
    This reverts commit f376b9910c698755cca418fea0d7750c988a7f07.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,70 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        meta: {
-            banner: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> */\n'
-        },
-		uglify: {
-            options: {
-                banner: '<%= meta.banner %>'
-            },
-            build: {
-                src: 'src/test.js',
-                dest: 'bulid/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
-            }
-        },
-		jshint:{
-			build:['Gruntfile.js','src/*.js'],
-			/*t4s:['Gruntfile.js'],
-			t3s:['src/*.js'],*/
-			options:{
-				jshintrc:'.jshintrc'
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		meta : {
+			banner : '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> */\n'
+		},
+		uglify : {
+			options : {
+				banner : '<%= meta.banner %>'
+			},
+			build : {
+				src : 'src/test.js',
+				dest : 'bulid/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
 			}
 		},
-		csslint:{
-			build:['Gruntfile.js','src/*.js'],
+		jshint : {
+			build : ['Gruntfile.js', 'src/*.js'],
 			/*t4s:['Gruntfile.js'],
 			t3s:['src/*.js'],*/
-			options:{
-				csslint:'..csslintrc'
+			options : {
+				jshintrc : '.jshintrc'
 			}
 		},
-		watch:{
-			build:{
-				files:['src/*.js','src/*.css'],
-				tasks:['jshint','uglify'],
-				options:{spawn:false}
+		csslint : {
+			build : ['Gruntfile.js', 'src/*.js'],
+			/*t4s:['Gruntfile.js'],
+			t3s:['src/*.js'],*/
+			options : {
+				csslint : '.csslintrc'
+			}
+		},
+		watch : {
+			build : {
+				files : ['src/*.js', 'src/*.css'],
+				tasks : ['jshint', 'uglify'],
+				options : {
+					spawn : false
+				}
+			}
+		},
+		concat : {
+			options : {},
+			dist : {
+				//src文件夹下包括子文件夹下的所有文件
+				src : ['src/**/*.js'],
+				//合并文件在dist下名为built.js的文件
+				dest : 'dist/built.js'
+			}
+		},
+		cssmin : { //css文件压缩
+			css : {
+				src : 'css/all.css', //将之前的all.css
+				dest : 'css/all.min.css' //压缩
 			}
 		}
-    });
+	});
 
-   // grunt.loadNpmTasks('grunt-unwrap');
-   grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-contrib-jshint');
-   grunt.loadNpmTasks('grunt-contrib-watch');
-   grunt.loadNpmTasks('grunt-contrib-csslint');
-   grunt.registerTask('default', ['jshint','uglify','watch','csslint']);
+	// grunt.loadNpmTasks('grunt-unwrap');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	//grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	grunt.loadNpmTasks('grunt-css');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['jshint', 'uglify', 'cssmain', 'csslint', 'concat']);
 
 };
